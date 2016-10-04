fd7913a65a55b9ffffcfe08a45ca350355d326da
RomanDorosinec <mr.dorosinec@mail.ru>
2016-10-04 14:17:37 +0300

    task 4 latest update

--- taskfour/src/by/dorosinets/training/quadraticequation/Equation.java
+++ taskfour/src/by/dorosinets/training/quadraticequation/Equation.java
@@ -5,31 +5,29 @@ public class Equation {
     private double numberA;
     private double numberB;
     private double numberC;
+    // Amount of numbers after '.'
+    public final static int MAX_SIZE = 4;
 
     public Equation(double a, double b, double c) {
-        
+        this.numberA = a;
+        this.numberB = b;
+        this.numberC = c;
     }
-
-    public double getNumberA() {
-        return numberA;
-    }
-    public double getNumberB() {
-        return numberB;
-    }
-    public double getNumberC() {
-        return numberC;
-    }
-    public double rootEquation () {
+    // Method count descriminant
+    public String rootEquation () {
         double discriminant;
         double rootOne;
         double rootTwo;
+        if (numberA == 0.0) {
+            return "This is not a quadratice Equation";
+        }
         discriminant = numberB*numberB - 4 * numberA * numberC;
         rootOne = (-numberB + sqrt(discriminant)) / 2 * numberA;
         rootTwo = (-numberB - sqrt(discriminant)) / 2 * numberA;
         if (0 != discriminant) {
-            System.out.println("rootOne: " + rootOne + "\nrootTwo: " + rootTwo);
-        }
-        else System.out.println("Discriminant is zero. Equation has one root.\nSingl root: " + rootOne);
-        return 0;
+            return "rootOne: " + rootOne + "\nrootTwo: " + rootTwo;
+        } else
+            return "Discriminant is zero. Equation has one root.\nSingle root: " + rootOne;
+
     }
 }
--- taskfour/src/by/dorosinets/training/quadraticequation/Runner.java
+++ taskfour/src/by/dorosinets/training/quadraticequation/Runner.java
@@ -1,13 +1,32 @@
 package by.dorosinets.training.quadraticequation;
-
+// This class is an entry point in the program
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Quadratic equation: " + args[0]+ "x^2 +(" + args[1] + ")x + " + args[2] + " = 0");
-        Equation equation = new Equation(
-                Double.parseDouble(args[0]),
-                Double.parseDouble(args[1]),
-                Double.parseDouble(args[2]));
+        // Helps to look after amount of numbers after '.'
+        boolean flag = true;
+        try {
+            for (int i = 0; i<args.length; i++) {
+                // Looks for '.' in parameter, it means, define if number is integer
+                if (args[i].indexOf('.') != -1)
+                    // Define the length of string after '.' and compare it to MAX_SIZE
+                    if (args[i].substring(args[i].indexOf('.')).length() > Equation.MAX_SIZE)
+                        flag = false;
+            }
 
-        System.out.println(equation.rootEquation());
+            if (flag) {
+                System.out.println("Quadratic equation: " + args[0] + "x^2 +(" + args[1] + ")x + " + args[2] + " = 0");
+                Equation equation = new Equation(
+                        Double.parseDouble(args[0]),
+                        Double.parseDouble(args[1]),
+                        Double.parseDouble(args[2]));
+                System.out.println(equation.rootEquation());
+            }
+            else
+                System.out.println("Not valid amount of numbers after .");
+            }catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Empty parameters");
+            }catch (NumberFormatException e) {
+                System.out.println("Not valid format number");
+            }
     }
 }
