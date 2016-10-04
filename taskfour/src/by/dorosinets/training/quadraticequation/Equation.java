c704c8f6f757017a3d9d0fe1f152a491007f2253
RomanDorosinec <mr.dorosinec@mail.ru>
2016-10-04 09:43:56 +0300

    Task 4

---
+++ taskfour/src/by/dorosinets/training/quadraticequation/Equation.java
@@ -0,0 +1,44 @@
+package by.dorosinets.training.quadraticequation;
+import static java.lang.Math.*;
+
+public class Equation {
+    private double numberA;
+    private double numberB;
+    private double numberC;
+
+    public Equation(double a, double b, double c) {
+        
+    }
+
+    public double getNumberA() {
+        return numberA;
+    }
+    public void setNumberA(double numberA) {
+        this.numberA = numberA;
+    }
+    public double getNumberB() {
+        return numberB;
+    }
+    public void setNumberB(double numberB) {
+        this.numberB = numberB;
+    }
+    public double getNumberC() {
+        return numberC;
+    }
+    public void setNumberC(double numberC) {
+        this.numberC = numberC;
+    }
+    public double rootEquation () {
+        double discriminant;
+        double rootOne;
+        double rootTwo;
+        discriminant = numberB*numberB - 4 * numberA * numberC;
+        rootOne = (-numberB + sqrt(discriminant)) / 2 * numberA;
+        rootTwo = (-numberB - sqrt(discriminant)) / 2 * numberA;
+        if (0 != discriminant) {
+            System.out.println("rootOne: " + rootOne + "\nrootTwo: " + rootTwo);
+        }
+        else System.out.println("Discriminant is zero. Equation has one root.\nSingl root: " + rootOne);
+        return 0;
+    }
+}
