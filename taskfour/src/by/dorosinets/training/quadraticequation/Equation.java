6eaedda1c416a52612040bc009131488daa918c8
RomanDorosinec <mr.dorosinec@mail.ru>
2016-10-05 17:39:20 +0300

    task 4 quadraticequation finish version

--- taskfour/src/by/dorosinets/training/quadraticequation/Equation.java
+++ taskfour/src/by/dorosinets/training/quadraticequation/Equation.java
@@ -1,62 +1,36 @@
 package by.dorosinets.training.quadraticequation;
+import java.math.BigDecimal;
+
 import static java.lang.Math.*;
 
 /**
  * Class for working with equations
  */
 public class Equation {
-    private double numberA;
-    private double numberB;
-    private double numberC;
-    // Amount of numbers after '.'
-    public final static int MAX_SIZE = 4;
-
-    /**
-     * Creates new equation
-     * Tests for values NaN
-     * @param a coefficient near x^2
-     * @param b coefficient near x
-     * @param c free coefficient
-     */
-    public Equation(double a, double b, double c) {
-        if (isNumberValid(a) || isNumberValid(b) || isNumberValid(c)) {
-            throw new ArithmeticException("Coefficients should not be NaN or infinite");
-        }
-        this.numberA = a;
-        this.numberB = b;
-        this.numberC = c;
-    }
-
     /**
      * Calculates equation roots
      * @return a string with equation solution - no roots, one root or two roots
+     * @param numberA coefficient near x^2
+     * @param numberB coefficient near x
+     * @param numberC free coefficient
      */
-    public String rootEquation() {
+    public static String rootEquation(BigDecimal numberA, BigDecimal numberB, BigDecimal numberC) {
         // Handles the case when coefficient A is zero
-        if (numberA == 0.0) {
+        if (numberA.compareTo(BigDecimal.ZERO) == 0) {
             return "This is not a quadratice Equation";
         }
-        double discriminant = numberB * numberB - 4 * numberA * numberC;
+        BigDecimal discriminant = numberB.pow(2).subtract(BigDecimal.valueOf(4).multiply(numberA).multiply(numberC));
         // Handles the case when discriminant less then zero
-        if (discriminant < 0) {
+        if (discriminant.signum() < 0) {
             return "Discriminant is less than zero. The roots of no";
         }
-        double rootOne = (-numberB + sqrt(discriminant)) / 2 * numberA;
-        double rootTwo = (-numberB - sqrt(discriminant)) / 2 * numberA;
+        BigDecimal rootOne = numberB.negate().add(BigDecimal.valueOf(Math.sqrt(discriminant.doubleValue()))).divide(BigDecimal.valueOf(2)).multiply(numberA);
+        BigDecimal rootTwo = numberB.negate().subtract(BigDecimal.valueOf(Math.sqrt(discriminant.doubleValue()))).divide(BigDecimal.valueOf(2)).multiply(numberA);
         // Event processing when the discriminant is zero
-        if (discriminant != 0) {
+        if (discriminant.signum() != 0) {
             return "rootOne: " + rootOne + "\nrootTwo: " + rootTwo;
         } else {
             return "Discriminant is zero. Equation has one root.\nSingle root: " + rootOne;
         }
-    }
-
-    /**
-     * Tests for values NaN and infinite
-     * @param number
-     * @return
-     */
-    public static boolean isNumberValid(double number) {
-        return Double.isNaN(number) || Double.isInfinite(number);
     }
 }
--- taskfour/src/by/dorosinets/training/quadraticequation/Runner.java
+++ taskfour/src/by/dorosinets/training/quadraticequation/Runner.java
@@ -1,5 +1,6 @@
 package by.dorosinets.training.quadraticequation;
 
+import java.math.BigDecimal;
 /**
  * This class is an entry point in the program
  * Receives and processes the command line arguments
@@ -8,19 +9,20 @@ public class Runner {
     /**
      * It receives and processes the command line arguments
      * Handle exceptions
+     *
      * @param args params that take from the command line
      */
     public static void main(String[] args) {
         try {
             // Out our quadratic equation
             System.out.println("Quadratic equation: " + args[0] + "x^2 +(" + args[1] + ")x + " + args[2] + " = 0");
-            Equation equation = new Equation(
-                    Double.parseDouble(args[0]),
-                    Double.parseDouble(args[1]),
-                    Double.parseDouble(args[2]));
-            System.out.println(equation.rootEquation());
-            // Exception processing if not entered elements
+            System.out.println(Equation.rootEquation(
+                    new BigDecimal(args[0]),
+                    new BigDecimal(args[1]),
+                    new BigDecimal(args[2])
+            ));
         } catch (ArrayIndexOutOfBoundsException e) {
+            // Exception processing if not entered elements
             System.out.println("Empty parameters");
             // Exception processing if entered string
         } catch (NumberFormatException e) {
