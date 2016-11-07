fdb43fde62c3bc5393dff572b8db3c86079a021a
RomanDorosinec <mr.dorosinec@mail.ru>
2016-11-07 14:35:24 +0300

    Triangle_modify + tests

--- taskFive/src/by/dorosinec/traning/triangle/Runner.java
+++ taskFive/src/by/dorosinec/traning/triangle/Runner.java
@@ -1,29 +1,30 @@
 package by.dorosinec.traning.triangle;
 
 import java.math.BigDecimal;
+import java.util.InputMismatchException;
+
 /**
- * This class is an entry point in the program
- * Receives and processes the command line arguments
+ * This class is an entry point in the program. Get sides from console
+ * and print type of the triangle
  */
 public class Runner {
+    private final static int NUMBER_OF_SIDES = 3;
+
     /**
-     * It receives and processes the command line arguments
+     * Entry point in the program
      * Handle exceptions
+     *
      * @param args params that take from the command line
      */
     public static void main(String[] args) {
         try {
-            System.out.println(Triangle.getTriangleType(
-                    new BigDecimal(args[0]),
-                    new BigDecimal(args[1]),
-                    new BigDecimal(args[2])
-            ));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            // Exception processing if not entered elements
-            System.out.println("Empty parameters");
-            // Exception processing if entered string
-        } catch (NumberFormatException e) {
-            System.out.println("Not valid format number");
+            BigDecimal[] inputSides = new InputParams().getSides(NUMBER_OF_SIDES);
+            Triangle triangle = new Triangle(inputSides[0], inputSides[1], inputSides[2]);
+            System.out.println(triangle.getTriangleType());
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.out.println("Not valid format input data");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
